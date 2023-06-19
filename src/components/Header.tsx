4481ec5c08cdb7b4086a5d9e1efbac52f5687ecc
JUMTBOX <yjey12@naver.com>
2023-06-19 22:16:34 +0900

    settings

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <nav className="header">
+      <button>내 이력서</button>
+    </nav>
+  );
+}
