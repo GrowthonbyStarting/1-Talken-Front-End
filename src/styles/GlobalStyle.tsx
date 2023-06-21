2d0f80cba4256d434ca7217210aac311fe3a9661
JUMTBOX <yjey12@naver.com>
2023-06-21 23:10:23 +0900

    ...

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -2,7 +2,7 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
     body {
-        background-color: white;
+        background-color: whitesmoke;
         margin: 0;
     }
 `;
